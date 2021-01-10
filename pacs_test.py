a0ff505e189ffb658c3ed6f022c0b2d5996e4c5b
Flavio Camus <chicuco@gmail.com>
2021-01-10 18:17:11 -0300

    basic directory structure

--- pacs_test.py
+++ pacs_test.py
@@ -1,14 +1,22 @@
 from pydicom.filewriter import write_file_meta_info
 
 from pynetdicom import AE, evt, AllStoragePresentationContexts
-
-
-
+import os
 
 # Implement a handler for evt.EVT_C_STORE
 def handle_store(event):
     """Handle a C-STORE request event."""
-    with open(event.request.AffectedSOPInstanceUID, 'wb') as f:
+    PatientID=event.dataset.PatientID
+    StudyID = event.dataset.StudyInstanceUID
+    #check and create directories
+    # Structure: PATIENT -> Study
+    if not os.path.exists('DICOMDIR/'+PatientID):
+        os.makedirs('DICOMDIR/'+PatientID)
+    if not os.path.exists('DICOMDIR/'+PatientID+'/'+StudyID):
+        os.makedirs('DICOMDIR/'+PatientID+'/'+StudyID)
+    #TODO check for errors, disk size avaliable
+    with open('DICOMDIR/'+PatientID+'/'+StudyID+'/'+event.request.AffectedSOPInstanceUID, 'wb') as f:
+        #TODO lookup for compression
         # Write the preamble and prefix
         f.write(b'\x00' * 128)
         f.write(b'DICM')
@@ -16,6 +24,9 @@ def handle_store(event):
         write_file_meta_info(f, event.file_meta)
         # Write the encoded dataset
         f.write(event.request.DataSet.getvalue())
+        #Write success? --- 
+        #TODO Sadd to DB : Patients,Studies,Series,Images
+
 
     # Return a 'Success' status
     return 0x0000
